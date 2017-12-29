0ff94e49d310fdf16fee1fb12324c0b62de8c232
dryyun <dryyun@gmail.com>
2017-12-29 15:35:15 +0800

    try use screen and process modules

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const electron = require('electron');
+
+// 定时查看内存使用
+(function () {
+    setInterval(function () {
+        let memoryUsed = process.getProcessMemoryInfo().workingSetSize / 1000;
+        $('#memoryUsed')[0].innerHTML = `Memory Used : ${memoryUsed} M`;
+    }, 2000)
+})();
+
+(function () {
+    let display = electron.screen.getPrimaryDisplay();
+    $('#screen')[0].innerHTML = `Screen width: ${display.size.width}，height: ${display.size.height}`
+    $('#screen').show();
+})();
